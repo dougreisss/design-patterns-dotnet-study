8cee08c1f0eb1c57e01b1d01baf95e9ea371e785
Douglas Reis <59988180+dougreisss@users.noreply.github.com>
2025-08-15 02:14:41 +0000

    :sparkles: feat: Singleton

---
+++ Singleton/Model/ConfigManager.cs
@@ -0,0 +1,32 @@
+﻿namespace Singleton.Model;
+
+public class ConfigManager
+{
+    private static ConfigManager _instance;
+    private Dictionary<string, string> _configs;
+
+    private ConfigManager() 
+    {
+        _configs = new Dictionary<string, string>();
+    }
+
+    public static ConfigManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new ConfigManager();
+        }
+
+        return _instance;
+    }
+
+    public void Set(string key, string value) 
+    {
+        _configs[key] = value;
+    }
+
+    public string Get(string key)
+    {
+        return _configs.ContainsKey(key) ? _configs[key] : null;
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿using Singleton.Model;
+
+ConfigManager config1 = ConfigManager.GetInstance();
+config1.Set("Ambiente", "Produção");
+
+ConfigManager config2 = ConfigManager.GetInstance();
+Console.WriteLine(config1.Get("Ambiente"));
+
+Console.WriteLine(object.ReferenceEquals(config1, config2)); // True
